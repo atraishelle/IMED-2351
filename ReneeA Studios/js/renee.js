aff526fd32fcbf8dc180a2c106405a0884537120
T. Renee Armstrong <35774849+atraishelle@users.noreply.github.com>
2018-04-25 10:13:33 -0500

    Final
    my website

--- ReneeA Studios/js/renee.js
+++ ReneeA Studios/js/renee.js
@@ -1,68 +1,33 @@
 // JavaScript Document
-//cookies
 
-function setCookie(cname,cvalue,exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires=" + d.toGMTString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+// When the user clicks on div, open the popup
+function myFunction3() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
 }
 
-function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-            return c.substring(name.length, c.length);
-        }
-    }
-    return "";
-}
-
-function checkCookie() {
-    var user=getCookie("username");
-    if (user != "") {
-        alert("Welcome again " + user);
+//menu buttons//
+function myFunction() {
+    var x = document.getElementById("myAbout");
+    if (x.style.display === "none") {
+        x.style.display = "block";
     } else {
-       user = prompt("Please enter your name:","");
-       if (user != "" && user != null) {
-           setCookie("username", user, 30);
-       }
+        x.style.display = "none";
     }
 }
-
-	
-	//
-	
-	
-var renee = document.getElementById("renee");
-var ill = document.getElementById("ill");
-var graph = document.getElementById("graph");
-var template = document.getElementById("template");
-
-renee.addEventListener("click", picLink);
-ill.addEventListener("click", picLink);
-graph.addEventListener("click", picLink);
-template.addEventListener("click", picLink);
-
-function picLink() {
-  var allImages = document.querySelectorAll("img");
-
-  for (var i = 0; i < allImages.length; i++) {
-    allImages[i].className = "hide";
-  }
-
-  var picId = this.attributes["data-img"].value;
-  var pic = document.getElementById(picId);
-  if (pic.className === "hide") {
-    pic.className = "";
-  } else {
-    pic.className = "hide";
-  }
+function myFunction1() {
+    var x = document.getElementById("myDesign");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
 }
-
+function myFunction2() {
+    var x = document.getElementById("container");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
